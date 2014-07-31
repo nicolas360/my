23b19e6d675190ec21620ceb2a9d96dbf3c334f5
nicolas360 <654460950@qq.com>
2014-07-31 15:26:56 +0800

    测试handler

---
+++ src/main/java/com/zhuc/my/netty/netty4/t4/NettyHandler.java
@@ -0,0 +1,41 @@
+package com.zhuc.my.netty.netty4.t4;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NettyHandler extends ChannelInboundHandlerAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(NettyHandler.class);
+
+	/* (non-Javadoc)
+	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelActive(io.netty.channel.ChannelHandlerContext)
+	 */
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		logger.debug("channelActive");
+
+		//		ByteBuf buf = Unpooled.buffer();
+		//		buf.writeBytes("[,,,,123]".getBytes());
+		//		ctx.writeAndFlush(buf);
+	}
+
+	/* (non-Javadoc)
+	 * @see io.netty.channel.ChannelInboundHandlerAdapter#channelInactive(io.netty.channel.ChannelHandlerContext)
+	 */
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		logger.debug("channelInactive");
+	}
+
+	/* (non-Javadoc)
+	 * @see io.netty.channel.ChannelInboundHandlerAdapter#exceptionCaught(io.netty.channel.ChannelHandlerContext, java.lang.Throwable)
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		logger.error("", cause);
+	}
+
+}
