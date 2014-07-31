dd50ad5d9d20d0f5d0885f04658b2250b6a54d17
nicolas360 <654460950@qq.com>
2014-07-31 15:24:19 +0800

    netty4重连

--- src/main/java/com/zhuc/my/netty/netty4/t4/NettyClient.java
+++ src/main/java/com/zhuc/my/netty/netty4/t4/NettyClient.java
@@ -1,6 +1,7 @@
 package com.zhuc.my.netty.netty4.t4;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -11,8 +12,6 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
@@ -24,12 +23,14 @@ public class NettyClient {
 	private final String host;
 	private final int port;
 
+	public Channel channel;
+
 	public NettyClient(String host, int port) {
 		this.host = host;
 		this.port = port;
 	}
 
-	public void run() throws Exception {
+	public void run() {
 		// Configure the client.
 		EventLoopGroup group = new NioEventLoopGroup();
 		try {
@@ -38,19 +39,21 @@ public class NettyClient {
 					.handler(new ChannelInitializer<SocketChannel>() {
 						@Override
 						public void initChannel(SocketChannel ch) throws Exception {
-							ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO)
-							//									new NettyHandler()
-									);
+							ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO), new NettyHandler());
 						}
 					});
 
 			// Start the client.	
-			ChannelFuture f = b.connect(host, port).sync();
+			ChannelFuture f = b.connect(host, port).sync(); //运行后会在这里阻塞, 连接成功往下运行, 若失败则抛出异常退出
+			logger.debug("1");
+
+			channel = f.channel();
 
 			// Wait until the connection is closed.
-			f.channel().closeFuture().sync();//运行后会在这里阻塞
+			channel.closeFuture().sync(); //运行后会在这里阻塞, 直到通道关闭才会往下走
+			logger.debug("2");
 		} catch (Exception e) {
-			logger.error("netty连接失败", e);
+			logger.error("服务器连接失败", e);
 
 		} finally {
 			// Shut down the event loop to terminate all threads.
@@ -59,7 +62,18 @@ public class NettyClient {
 	}
 
 	public static void main(String[] args) {
-		ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
-		service.scheduleAtFixedRate(new ReconnectTimer(), 1, 10, TimeUnit.SECONDS);
+		while (true) {
+			// 利用sync()阻塞特性, 这里可以死循环重连
+			logger.debug("开始连接！");
+			NettyClient nc = new NettyClient("192.168.70.12", 10100);
+			nc.run(); //到这里阻塞
+
+			try {
+				TimeUnit.SECONDS.sleep(5l);
+			} catch (InterruptedException e) {
+				logger.error("", e);
+			}
+		}
+
 	}
 }
